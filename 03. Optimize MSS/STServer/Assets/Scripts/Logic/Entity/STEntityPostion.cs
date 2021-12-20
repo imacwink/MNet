aedfe765973f0c150212cee66e7a171cf7b2fec5
macwink <417024106@qq.com>
2021-12-20 15:21:45 +0800

    Delete 03. Optimize MSS directory

--- 03. Optimize MSS/STServer/Assets/Scripts/Logic/Entity/STEntityPostion.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Entity
-{
-    public class STEntityPostion
-    {
-        public float X { get; set; }
-        public float Y { get; set; }
-        public float Z { get; set; }
-    }
-}
